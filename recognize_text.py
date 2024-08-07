475707559efdc8349d15d0431f04133e70299d3d
Fabio Brito <brito443@gmail.com>
2024-08-07 00:35:46 -0300

    New Image

--- recognize_text.py
+++ recognize_text.py
@@ -2,8 +2,8 @@ import requests
 import os
 
 # Configuração da API
-endpoint = "**************.cognitiveservices.azure.com/vision/v3.2/ocr"
-api_key = "9065ef9e61eb4c3ebeba3493*******"
+endpoint = "https://mycognitiveservices.cognitiveservices.azure.com/vision/v3.2/ocr"
+api_key = "9065ef9e61eb4c3ebeba3493c1a7ee63"
 headers = {
     "Ocp-Apim-Subscription-Key": api_key,
     "Content-Type": "application/octet-stream"
